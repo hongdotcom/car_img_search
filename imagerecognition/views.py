f5792a240d5996b1c570d58e7ce09f31ea00aa7a
hongdotcom <hongdotcom@gmail.com>
2021-01-28 16:46:09 +1300

    minimal file upload workable

---
+++ imagerecognition/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+
+# Create your views here.
+
+def home(request):
+  if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        return render(request, 'home.html', {
+            'uploaded_file_url': uploaded_file_url
+        })
+  return render(request, 'home.html')
+
