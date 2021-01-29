6583808f9631cd5c8879bfaa61d9a8193a1f10ee
hongdotcom <hongdotcom@gmail.com>
2021-01-29 17:31:22 +1300

    detect car labels workable, no UI yet

--- imagerecognition/analysis.py
+++ imagerecognition/analysis.py
@@ -2,25 +2,48 @@ import boto3
 
 def detect_labels_local_file(photo):
 
-
+    #define service used
     client=boto3.client('rekognition')
-   
+    #define result varible
+    resp = []
+
+    #Open image file
     with open(photo, 'rb') as image:
         response = client.detect_labels(Image={'Bytes': image.read()})
-        
-    print('Detected labels in ' + photo)    
+    #debug resposes 
+    # print('Detected labels in ' + photo)    
+    # print(response['Labels'])
+
+    #Iterate thru all detected labels
     for label in response['Labels']:
+        #debug msg 
         print (label['Name'] + ' : ' + str(label['Confidence']))
+        #Since result set is displayed by highest possbility first. We decided to only take the most possible result only. 
+        #So if the result is empty, add the highest possble one
+        if possible_outcome(label) != 'NOT Matched':
+            if not resp:
+                resp.append(label['Name'] + ' : ' + str(label['Confidence']))
+                # print(resp)
+    #if there is no detected label found, then return no match.
+    if not resp:
+        return "No Match"
+    return resp
 
-    return len(response['Labels'])
+#Function to detect the label that we want to file. In this case car search 
+def possible_outcome(label):
+    switcher={
+        'Truck':'Truck',
+        'Wagon': 'Wagon',
+        'Sedan':'Sedan',
+        'Coupe': 'Coupe',
+        'Hatchback': 'Hatchback',
+        'Van': 'Van',
+        'Suv': 'Suv',
+        'Pickup Truck': 'Pickup Truck',
+        'Convertible':'Convertible',
 
-def main():
-    photo='/Users/hongdotcom/Desktop/Sites/django-imgsearch/imgsearch/media/bg1.jpg'
-
-    label_count=detect_labels_local_file(photo)
-    print("Labels detected: " + str(label_count))
-
-
-if __name__ == "__main__":
-    main()
+    }
+    #debug messag
+    # print(switcher.get(label['Name'],'NOT Matched'))
+    return switcher.get(label['Name'],'NOT Matched')
 
--- imagerecognition/views.py
+++ imagerecognition/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
+from .analysis import detect_labels_local_file
 
 # Create your views here.
 
@@ -10,8 +11,10 @@ def home(request):
         fs = FileSystemStorage()
         filename = fs.save(myfile.name, myfile)
         uploaded_file_url = fs.url(filename)
+        res = detect_labels_local_file('/Users/hongdotcom/Desktop/Sites/django-imgsearch/imgsearch/' + uploaded_file_url)
+        print(res)
         return render(request, 'home.html', {
-            'uploaded_file_url': uploaded_file_url
+            'uploaded_file_url': uploaded_file_url,'res': res
         })
   return render(request, 'home.html')
 
