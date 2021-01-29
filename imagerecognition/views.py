1962d7b5e5b8a312fceb50864fd02f9dfc231e4c
hongdotcom <hongdotcom@gmail.com>
2021-01-30 01:26:06 +1300

    add css empty upload check & display catagory uploaded

--- imagerecognition/views.py
+++ imagerecognition/views.py
@@ -6,10 +6,16 @@ from .analysis import detect_labels_local_file
 # Create your views here.
 
 def home(request):
-  if request.method == 'POST' and request.FILES['myfile']:
+  try:
+    if request.method == 'POST' and request.FILES['myfile']:
         myfile = request.FILES['myfile']
         fs = FileSystemStorage()
         filename = fs.save(myfile.name, myfile)
+        filesize = fs.size(filename)
+        print(filesize)
+        if filesize > 5000000:
+          res = "Too Big"
+          return render(request, 'home.html', {'res': res})
         uploaded_file_url = fs.url(filename)
         res = detect_labels_local_file('/Users/hongdotcom/Desktop/Sites/django-imgsearch/imgsearch/' + uploaded_file_url)
         # print(res)
@@ -20,5 +26,9 @@ def home(request):
           })
         if res:
           return render(request, 'home.html', {'res': res})
+  except: 
+    res = "Unknown"
+    return render(request, 'home.html', {'res': res})
+    
   return render(request, 'home.html')
 
